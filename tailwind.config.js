a11a47be249ce2d28908972b9c3c9cf817e2bd10
asal <asal.esmaeili.2000@gmail.com>
2026-01-04 01:41:22 +0330

    Inilize Project

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+export default {
+    content: [
+        "./index.html",
+        "./product.html",
+        "./cart.html",
+        "./checkout.html",
+        "./src/**/*.{js,html}"
+    ],
+    theme: {
+        extend: {
+            colors: {
+                coffee: '#3b2a23',
+                coffeeLight: '#f7f4f1',
+                accent: '#f2c17d'
+            }
+        }
+    },
+    plugins: []
+}
